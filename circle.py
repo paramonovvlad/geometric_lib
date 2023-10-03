730c1ce9e6726d4493be867460f6dbf549a88546
paramonovvlad <144106023+paramonovvlad@users.noreply.github.com>
2023-10-03 11:16:48 +0300

    docs: комментарии к каждой функции в файле circle.py добавлены

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    # принимает r, возвращает r^2
     return math.pi * r * r
-
+	
 
 def perimeter(r):
+    # принимает r, возвращает 2*r
     return 2 * math.pi * r
 
