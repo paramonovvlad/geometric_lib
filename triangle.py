406fd473e2a16139a11f261a8d260d9000426cf2
paramonovvlad <144106023+paramonovvlad@users.noreply.github.com>
2023-10-03 11:17:46 +0300

    docs: комментарии к каждой функции в файле triangle.py добавлены

--- triangle.py
+++ triangle.py
@@ -1,6 +1,8 @@
 
 def area(a, h): 
+    # принимает a , h , возвращает произведение a и h , деленное на 2
     return a * h / 2 
 
-def perimeter(a, b, c): 
+def perimeter(a, b, c):
+    # принимает a, b, c, возвращает их сумму 
     return a + b + c 
