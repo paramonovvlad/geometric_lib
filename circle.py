aae6073267bd53c47f9416a4f67d20273452bf4d
paramonovvlad <144106023+paramonovvlad@users.noreply.github.com>
2023-10-04 23:28:19 +0300

    docs: readme изменен

--- circle.py
+++ circle.py
@@ -2,11 +2,11 @@ import math
 
 
 def area(r):
-    # принимает r, возвращает r^2
+    # принимает r, возвращает r^2*ПИ
     return math.pi * r * r
 	
 
 def perimeter(r):
-    # принимает r, возвращает 2*r
+    # принимает r, возвращает 2*r*ПИ
     return 2 * math.pi * r
 
